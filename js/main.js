45f0f4a5ded243a7ec2ba9cf531664bcfb0aca77
i8rice <chaiyamma@gmail.com>
2017-07-03 10:56:06 +0930

    test touchsmart

--- js/main.js
+++ js/main.js
@@ -169,6 +169,19 @@
   
   });
   
+  // Hover touch screen
+  $('ul.portfolio-image li').on('touchstart', function (e) {
+    'use strict'; //satisfy code inspectors
+    var link = $(this); //preselect the link
+    if (link.hasClass('hover')) {
+        return true;
+    } else {
+        link.addClass('hover');
+        $('a.taphover').not(this).removeClass('hover');
+        e.preventDefault();
+        return false; //extra, and to make sure the function has consistent return points
+    }
+  });
     
   //header
   function inits() {
@@ -205,3 +218,4 @@
   }
 
 })(jQuery);
+
